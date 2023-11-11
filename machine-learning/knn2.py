6351f198f54f5aa9f4036e0d85efad70cee47e21
lovetoseehappiness <92204047+lovetoseehappiness@users.noreply.github.com>
2023-11-11 07:04:34 -0800

    Create knn2.py

---
+++ machine-learning/knn2.py
@@ -0,0 +1,47 @@
+def dist(x1,×2):
+
+return np.sqrt(sum((x1-×2)**2))
+
+
+
+# Test Time
+
+def knn (X, Y, queryPoint, k=5):
+
+
+
+vals = []
+
+m = X. shape [0]
+
+
+
+for i in range(m):
+
+d = dist(queryPoint, X[i])
+
+vals.append( (d, Y [1]))
+
+
+
+vals = sorted(vals)
+
+# Nearest/First K points
+
+vals = vals[:k]
+
+
+
+vals = np.array (vals)
+
+
+new_vals = np. unique(vals [:, 11, return_counts=True)
+
+
+index = new_vals (1]. argmax ()
+
+pred = new_vals [0] (index]
+
+
+
+return pred
